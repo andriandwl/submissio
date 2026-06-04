04353001b15e293054bd4f9ba3c8040483fc40ec
Bariton Music <andriandwiharyantoe@gmail.com>
2026-06-04 20:59:24 +0700

    feat: add basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './styles/style.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/Discuss">
       <React.StrictMode>
         <App />
       </React.StrictMode>
