dcd69beab9982bb44258bd96700fdeef315e929b
Bariton Music <andriandwiharyantoe@gmail.com>
2026-06-04 21:11:24 +0700

    feat: update vercel

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './styles/style.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/Discuss">
+    <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
